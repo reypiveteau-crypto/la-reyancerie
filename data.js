f6c0a7f0b65b58da4cfd2d49ec47c42f0646c8d7
reypiveteau-crypto <rey.piveteau@gmail.com>
2026-09-13 22:22:31 +0200

    Update data.js

--- data.js
+++ data.js
@@ -38,9 +38,9 @@
    Un id absent de cette liste = carte normale, sans image. Rien ne casse.
    ============================================================ */
 const AVEC_IMAGE = [
-  // ajoute ici l'id d'un personnage pour afficher son image :  "mavuika", "hu-tao"
+  "albedo"// ajoute ici l'id d'un personnage pour afficher son image :  "mavuika", "hu-tao"
 ];
-"albedo"
+
 const VERSION_JEU = "7.0";
 const MAJ = "Septembre 2026";
 
