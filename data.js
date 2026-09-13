bd911c34349d4631acd75f7c7d0850b1edd3b1ec
reypiveteau-crypto <rey.piveteau@gmail.com>
2026-09-13 21:29:19 +0200

    Update data.js

--- data.js
+++ data.js
@@ -38,7 +38,7 @@
    Un id absent de cette liste = carte normale, sans image. Rien ne casse.
    ============================================================ */
 const AVEC_IMAGE = [
-  "diona.jpg"
+  
 ];
 
 const VERSION_JEU = "7.0";
